7b824c20f1ab442dee0320536e538e1a6e958c01
NerlyTon <nerlyton@gmail.com>
2020-09-12 11:14:19 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
@@ -23,12 +24,13 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/recipes/:id/edit' do  #load edit form
+  get '/recipes/:id/edit' do 
+
     @recipe = Recipe.find_by_id(params[:id])
     erb :edit
   end
  
-  patch '/recipes/:id/' do #edit action
+  patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
