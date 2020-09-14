a34c3c37acf9e44fae7c668804eeac4457a48eec
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-14 10:29:02 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect to "/recipes"
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :"recipes/new"
+  end
+
+  get '/recipes/:id' do
+    current_recipe
+    erb :'recipes/show'
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create(params)
+    redirect to "/recipes/#{recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe.find_by_id(params[:id]).destroy
+    redirect to "/recipes"
+  end
+
+  get '/recipes/:id/edit' do
+    current_recipe
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+    current_recipe
+    @recipe.update(concise_params)
+    redirect to 'recipes/#{@recipe.id}'
+  end
+
+
+  helpers do
+    
+    def current_recipe
+      @recipe = Recipe.find_by_id(params[:id])
+    end
+
+    def concise_params
+      {name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time]}
+    end
+
+  end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+"INSERT INTO recipes (name, ingredients, cook_time) VALUES ('quesadilla', 'tortilla, cheese', '5 minutes');"
+"INSERT OR REPLACE INTO recipes (name, ingredients, cook_time) VALUES ('burger', 'burger meat, bun', '15 minutes');"
